1a946d33b0e85b63d4eda562bc3c748c20e4edfd
Martin Klomp <m.klomp@tudelft.nl>
2026-02-24 20:23:11 +0100

    Added wifi network settings

--- vue-frontend/pages/settings.vue
+++ vue-frontend/pages/settings.vue
@@ -91,11 +91,7 @@
 
     <div class="col-4 h-100 p-2">
       <div class="row">
-        <div class="col-12">
-          <div class="row">
-            <!-- <Network/>-->
-          </div>
-        </div>
+          <Network/>
       </div>
     </div>
   </div>
