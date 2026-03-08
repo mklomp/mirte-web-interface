b7983d22a08e9e3a10dbbe29489adf448b1ef0d8
Martin Klomp <m.klomp@tudelft.nl>
2026-03-08 19:54:32 +0100

    Fixed codemirror to sync with blokcly

--- vue-frontend/components/Codemirror.vue
+++ vue-frontend/components/Codemirror.vue
@@ -4,65 +4,59 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref, onMounted } from 'vue'
 import { EditorView, basicSetup } from "codemirror"
 import { python } from "@codemirror/lang-python"
+import { useCodeStore } from "@/stores/user_code"
+
+const props = defineProps({
+  visible: Boolean
+})
 
 const editorContainer = ref(null)
+const editor = ref(null)
 
+const codeStore = useCodeStore()
 
+onMounted(() => {
 
-export default {
-  props: ['visible'],
-  data: () => ({
-    editor: Object
-  }),
-  methods: {
-    makeMarker: () => {
-      var marker = document.createElement("div");
-      marker.style.color = "#822";
-      marker.innerHTML = "⇨";
-      return marker;
-    }
-  },
-  mounted() {
-    this.editorContainer = this.$refs.editorContainer
-    const codeStore = useCodeStore()
+  editor.value = new EditorView({
+    parent: editorContainer.value,
+    doc: codeStore.python,
 
-    this.editor = new EditorView({
-      parent: this.editorContainer,
-      doc: codeStore.python, // initial value from store
-      extensions: [
-        basicSetup,
-        python(),
-        EditorView.updateListener.of(update => {
-          if (update.docChanged) {
-            // update store whenever user types
-            codeStore.setPython(update.state.doc.toString())
-          }
-        })
-      ]
-    })
+    extensions: [
+      basicSetup,
+      python(),
 
-  },
-  watch: {
-    visible(newVal) {
-      if (newVal && this.editor) {
-        this.$nextTick(() => {
-          //this.editor.refresh();
-        });
-      }
-    },
-    '$store.getters.getCode': function(newVal, oldVal) {
-       if (newVal != this.editor.getDoc().getValue()){
-          this.editor.setValue(newVal)
-       }
-    },
-    '$store.getters.getLinenumber': function(newVal, oldVal){
-      this.editor.clearGutter("linetracer");
-      this.editor.setGutterMarker(newVal-1, "linetracer", this.makeMarker());
+      EditorView.updateListener.of(update => {
+        if (update.docChanged && props.visible) {
+          codeStore.setPython(update.state.doc.toString())
+        }
+      })
+    ]
+  })
+
+})
+
+watch(
+  () => codeStore.python,
+  (newCode) => {
+
+    if (!editor.value || props.visible) return
+
+    const current = editor.value.state.doc.toString()
+
+    if (current !== newCode) {
+      editor.value.dispatch({
+        changes: {
+          from: 0,
+          to: current.length,
+          insert: newCode
+        }
+      })
     }
   }
-}
+)
+
 </script>
